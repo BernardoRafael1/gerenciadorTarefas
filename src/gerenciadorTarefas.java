60b68f693dd543922798e9083c17a42f9ccfbf90
BernardoRafael1 <bernardorafael.achaves@gmail.com>
2026-03-05 17:55:49 -0300

    Adição de comentários e função de remover uma tarefa

--- src/gerenciadorTarefas.java
+++ src/gerenciadorTarefas.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class gerenciadorTarefas {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         ArrayList<String> tarefas = new ArrayList<>();
         int opcao = 0;
@@ -12,8 +12,9 @@ public class gerenciadorTarefas {
         do {
             System.out.println("\n1. Adicionar Tarefa");
             System.out.println("2. Listar Tarefas");
-            System.out.println("3. Sair");
-            System.out.print("Escolha: ");
+            System.out.println("3. Remover Tarefa");
+            System.out.println("4. Sair");
+            System.out.print("Escolha: "); // menu
 
             opcao = leitor.nextInt();
             leitor.nextLine();
@@ -22,7 +23,7 @@ public class gerenciadorTarefas {
                 System.out.print("Digite a tarefa: ");
                 String novaTarefa = leitor.nextLine();
                 tarefas.add(novaTarefa);
-                System.out.println("Tarefa salva!");
+                System.out.println("Tarefa salva!"); // permite inserir uma tarefa
             } else if (opcao == 2) {
                 System.out.println("\n Suas Tarefas");
 
@@ -30,12 +31,33 @@ public class gerenciadorTarefas {
                     System.out.println("Sua lista está vazia no momento.");
                 } else {
                     for (int i = 0; i < tarefas.size(); i++) {
-                        System.out.println((i+1) + ". " + tarefas.get(i));
+                        System.out.println((i + 1) + ". " + tarefas.get(i));
+                    } // salva as tarefas na lista de tarefas de fato
+                }
+
+            } else if (opcao == 3) {
+                if (tarefas.isEmpty()) {
+                    System.out.println("A lista está vazia, nada para remover.");
+                } else {
+                    System.out.println("Lista de Tarefas");
+                    for (int i = 0; i < tarefas.size(); i++) {
+                        System.out.println((i + 1) + ". " + tarefas.get(i));
+
+                        System.out.print("\nQual número deseja remover? ");
+                        int idRemover = leitor.nextInt();
+                        leitor.nextLine();
+
+                        if (idRemover > 0 && idRemover <= tarefas.size()) {
+                            String removida = tarefas.remove(idRemover - 1);
+                            System.out.println("Tarefa '" + removida + "' removida!");
+                        } else {
+                            System.out.println("Número inválido."); // aqui ele remove uma das tarefas de fato
+                        }
                     }
                 }
             }
-        } while (opcao != 3);
+        } while (opcao != 4);
 
-        System.out.println("Encerrando... Bom dia de trabalho!");
+        System.out.println("Encerrando... Bom dia de trabalho!"); // se o usuário selecionar a opção 4 o programa encerra
     }
 }
